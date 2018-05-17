d04fb851cb7c0cf97ee874db3d26f6768ed11cbd
smileboywtu <smileboywtu@gmail.com>
2018-05-17 10:10:48 +0800

    add fake module

--- colly/encoder_test.go
+++ colly/encoder_test.go
@@ -1,14 +1,33 @@
 package colly
 
 import "testing"
+import "github.com/icrowley/fake"
 
-func BenchmarkFileContentEncoder_Encode(b *testing.B) {
+func testFileContentEncoder_Encode(data []byte, b *testing.B) {
 
 	encoder := FileContentEncoder{
-		FilePath: "/tmp/a.txt",
-		FileContent:[]byte{'a', 'b', 'c'},
+		FilePath:    "/tmp/a.txt",
+		FileContent: data,
 	}
-	for i:=0 ; i<b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		encoder.Encode()
 	}
+}
+
+func BenchmarkFileContentEncoder_Encode10(b *testing.B) {
+	testFileContentEncoder_Encode([]byte(fake.CharactersN(10)), b)
+}
+
+func BenchmarkFileContentEncoder_Encode20(b *testing.B) {
+	testFileContentEncoder_Encode([]byte(fake.CharactersN(20)), b)
+}
+
+func BenchmarkFileContentEncoder_Encode30(b *testing.B) {
+	testFileContentEncoder_Encode([]byte(fake.CharactersN(30)), b)
+}
+func BenchmarkFileContentEncoder_Encode40(b *testing.B) {
+	testFileContentEncoder_Encode([]byte(fake.CharactersN(40)), b)
+}
+func BenchmarkFileContentEncoder_Encode50(b *testing.B) {
+	testFileContentEncoder_Encode([]byte(fake.CharactersN(50)), b)
 }
